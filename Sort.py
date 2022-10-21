00c0ade576f39517b924176efa6aff922ad60aec
Jean Guiraud <97190517+JeanGrd@users.noreply.github.com>
2022-10-21 17:21:14 +0200

    Add files via upload

---
+++ Sort.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 21 15:39:26 2022
+
+@author: Guiraud Jean
+"""
+
+list1 = ["z_4", 'ok_2', 'ok_1', 'mag_34', "z_2", 'ok_3', 'z_9', 'o_2', 'qsmdlkjf_14', 'o_18', 'z_960', 'GAL-MGT-ESA-GMS-X-1788_3.4_GALILEO FOC2 GROUND MISSION SEGMENT AND GALILEO SECURITY FACILITY CONTRACTUAL INDEX STATUS LIST (CISL)"', 'GAL-MGT-ESA-GMS-X-1788_3.2_GALILEO FOC2 GROUND MISSION SEGMENT AND GALILEO SECURITY FACILITY CONTRACTUAL INDEX STATUS LIST (CISL)']
+list1.sort()
+
+print(list1)
+
+list2 = []
+new_list = []
+
+val = 0
+
+for val in range(len(list1) - 1) :
+
+    cut_string1 = list1[val].split("_")
+    cut_string2 = list1[val+1].split("_")
+    
+    if cut_string1[0] != cut_string2[0]:
+        list2.append(list1[val])
+    elif val == len(list1) - 2 :
+        list2.append(list1[val+1])
+
+
+    
+print("---------------------------------------------")
+print(list2)
+    
