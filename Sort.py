c5f67c3b31da203909c6d13a5f5cacd2d8b1d588
Jean Guiraud <97190517+JeanGrd@users.noreply.github.com>
2022-10-24 11:41:37 +0200

    Update Sort.py

--- Sort.py
+++ Sort.py
@@ -4,7 +4,7 @@
 ---------------------------------------------------------------------------------------------------------------
 
 Remark : This code is just a test function for Moko application
-It permits to keep the highest issue from a list where issue is
+It allows to keep the highest issue from a list where issue is
 located in a text box with separators, in my case, the separator
 is '_'. 
 
