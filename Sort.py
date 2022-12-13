1b6cb024e3b172a98120728bb5b25b2b9f03fa9d
jean <jean.gd@outlook.fr>
2022-12-13 09:11:12 +0100

    New version

--- Sort.py
+++ Sort.py
@@ -18,13 +18,14 @@ Written by : Jean Guiraud
 # !/usr/bin/env python3
 # Python version : 3.8
 
-def just_highest_issues(splitter, n_splitter, list_to_split):
+def just_highest_issues(splitter, n_splitter, version, list_to_split):
     """
     
     Get the highest issue from a list
     
     @splitter = the separator
-    @n_splitter = the position of the issue name (example : issue_2 the position is 0)
+    @n_splitter = the position of the issue name (example : issue_2 the position is 0 : issue)
+    @version = the position of the version that you want to compare (example issue_2 is the position 1 : 2)
     @list_to_split = your list where you want to keep the highest issue
     
     """
@@ -37,10 +38,10 @@ def just_highest_issues(splitter, n_splitter, list_to_split):
         max = 0
         groupby = element = list_to_split[val].split(splitter)
 
-        while element[n_splitter-1] == groupby[n_splitter-1]:
+        while element[n_splitter] == groupby[n_splitter]:
 
-            if max < int(element[n_splitter]):
-                max = int(element[n_splitter])
+            if max < int(element[version]):
+                max = int(element[version])
                 maxInd = val
 
             val += 1
@@ -56,9 +57,9 @@ def just_highest_issues(splitter, n_splitter, list_to_split):
 
 
 if __name__ == "__main__":
-    list_test = ["z_4", 'ok_2', 'ok_1', 'mag_34', "z_2", 'ok_3', 'z_9', 'o_2321', 'qsmdlkjf_14', 'o_18',
-                 'z_960', "o_444"]  # Test list
+    list_test = ["z_4", 'ok_2', 'ok_1', 'mag_34', "z_2", 'ok_3', 'z_9', 'o_21', 'qsmdlkjf_14', 'o_18',
+                 'z_960', "o_5444"]  # Test list
 
     print(list_test)
     print("\n------------------------------------------------------\n")
-    print(just_highest_issues("_", 1, list_test))
+    print(just_highest_issues("_", 0, 1, list_test))
