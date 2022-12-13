4878156ecb2a8bd177ea128745a72d27c8a1018c
jean <jean.gd@outlook.fr>
2022-12-13 08:18:10 +0100

    New version

--- Sort.py
+++ Sort.py
@@ -14,11 +14,11 @@ Written by : Jean Guiraud
 
 """
 
+
 # !/usr/bin/env python3
 # Python version : 3.8
 
-def just_highest_issues(splitter, n_splitter, list_to_split): 
-    
+def just_highest_issues(splitter, n_splitter, list_to_split):
     """
     
     Get the highest issue from a list
@@ -28,30 +28,37 @@ def just_highest_issues(splitter, n_splitter, list_to_split):
     @list_to_split = your list where you want to keep the highest issue
     
     """
-    
+
     list_to_split.sort()
-    list_highest_issue = []    
-        
-    for val in range(len(list_to_split)) :
-                
-        cut_string1 = list_to_split[val].split(splitter)
-        
-        if val == len(list_to_split) - 1:
-            list_highest_issue.append(list_to_split[val])
-            break
-        
-        cut_string2 = list_to_split[val+1].split(splitter)
-        
-        if cut_string1[n_splitter] != cut_string2[n_splitter]:
-            list_highest_issue.append(list_to_split[val])
-         
+    list_highest_issue = []
+
+    val = 0
+    while val < len(list_to_split)-1:
+        max = 0
+        groupby = element = list_to_split[val].split(splitter)
+
+        while element[n_splitter-1] == groupby[n_splitter-1]:
+
+            if max < int(element[n_splitter]):
+                max = int(element[n_splitter])
+                maxInd = val
+
+            val += 1
+
+            if val > len(list_to_split)-1:
+                break
+
+            element = list_to_split[val].split(splitter)
+
+        list_highest_issue.append(list_to_split[maxInd])
+
     return list_highest_issue
 
-if __name__ == "__main__" :    
 
-    list_test = ["z_4", 'ok_2', 'ok_1', 'mag_34', "z_2", 'ok_3', 'z_9', 'o_2', 'qsmdlkjf_14', 'o_18', 'z_960'] # Test list
-    
-    print(list_test) 
+if __name__ == "__main__":
+    list_test = ["z_4", 'ok_2', 'ok_1', 'mag_34', "z_2", 'ok_3', 'z_9', 'o_2321', 'qsmdlkjf_14', 'o_18',
+                 'z_960', "o_444"]  # Test list
+
+    print(list_test)
     print("\n------------------------------------------------------\n")
-    print(just_highest_issues("_", 0, list_test))
-       
+    print(just_highest_issues("_", 1, list_test))
